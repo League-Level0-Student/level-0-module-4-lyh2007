3ce6689822e0fe3359077261f36b4771355993c4
An Idiot <iman@idi.ot>
2019-01-20 12:54:42 -0800

    lsgiEfhb;o
    ukjwyhv a8oisvflnwekfhvy;o8ifejknhv;iofevhoia35ghvn
    klvhdv;w9iajevr9pawjfkdnfhlfhv yrovlF#BFiocv ye;sflchv

--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -1,7 +1,7 @@
 package doubles_and_booleans;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
-//    Level 0
 
+//    Level 0
 
 import javax.swing.JOptionPane;
 
@@ -15,19 +15,20 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-String Change = JOptionPane.showInputDialog("how many nickels do you have? ");
+		String Change = JOptionPane.showInputDialog("how many nickels do you have? ");
 		// Convert their answer to an int using Integer.parseInt()
-        int C = Integer.parseInt(Change);
+		int C = Integer.parseInt(Change);
 		// Ask the user how many dimes they have, and convert their answer
-      String Dimes = JOptionPane.showInputDialog("how many dimes do you have?");
-   int D = Integer.parseInt(Dimes);
+		String Dimes = JOptionPane.showInputDialog("how many dimes do you have?");
+		int D = Integer.parseInt(Dimes);
 		// Ask the user how many quarters they have, and convert their answer
- String Quarters = JOptionPane.showInputDialog("how many quarters do you have?");
-int Q = Integer.parseInt(Quarters);
-		// Calculate how much money the user has and save it in a double variable 
-           double money=0.01*C+0.1*D+0.25*Q;   
+		String Quarters = JOptionPane.showInputDialog("how many quarters do you have?");
+		int Q = Integer.parseInt(Quarters);
+		// Calculate how much money the user has and save it in a double variable
+		double money = 0.01 * C + 0.1 * D + 0.25 * Q;
 		// Tell the user how much money they have
-            JOptionPane.showMessageDialog(null, "you have $"+money);
+		JOptionPane.showMessageDialog(null, "you have $" + money);
+		JOptionPane.showMessageDialog(null,
+				"I made you a figurative sandwich.(you said to make you a sandwich, you didn't specify which kind. :3)");
 	}
 }
-
