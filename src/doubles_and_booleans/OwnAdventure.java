ce29f8d9237c7088083a06edfa600d43bb31544f
PhoenixFeather101 <league@WTS-IM-17.attlocal.net>
2019-02-02 11:27:58 -0800

    Own adventure
    I started my own adventure project and saved it too!

---
+++ src/doubles_and_booleans/OwnAdventure.java
@@ -0,0 +1,16 @@
+package doubles_and_booleans;
+
+import javax.swing.JOptionPane;
+
+public class OwnAdventure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		JOptionPane.showMessageDialog(null, "You are in a dark haunted house, you hear a shriek. It sounds very much like your protecter/royal prince. You go to investigate, it is not your protecter/royal prince but your poor snooty cousin. Your protecter/Royal prince walks up to you and see what happened. He is not sad because he never liked your snooty cousin that much. He hated her as much as you do. He chopped her body up into many other peices and said, \n we will make her body into jerky and eatit later. ");
+	String dead =	JOptionPane.showInputDialog(null, "Your Cousin Beep is dead with her head cut off. There is also a puddle of blood around her. ");
+		
+	JOptionPane.showConfirmDialog(null, "Which one do you choose?" 'To make jerky out of beep');
+
+}
